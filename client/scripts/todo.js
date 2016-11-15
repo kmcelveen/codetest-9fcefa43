c66ba6a497c26f0932d78bf36929a346076a7118
Kristina McElveen <kmcelveen.sp26@gmail.com>
2016-11-14 18:30:54 -0800

    Todos load from server and user can add todo
    Add  Todos

--- client/scripts/todo.js
+++ client/scripts/todo.js
@@ -1,8 +1,47 @@
 $(document).ready(function(){
+  'use strict';
+    let $listItemTemplate = $('#list-item .item');
+    let $list = $('#list');
+
+    let $addItemToPage = function(itemData) {
+        let $item = $listItemTemplate.clone();
+        $item.attr('data-id', itemData.id);
+        $item.find('.description').text(itemData.description);
+        if (itemData.completed) {
+            $item.addClass('completed');
+        }
+        $list.append($item);
+    };
+    let $loadTodos = $.ajax({
+        type: 'GET',
+        url: 'https://listalous.herokuapp.com/lists/kmcelveen'
+    });
+    $loadTodos.done(function(data) {
+        let $itemsData = data.items;
+
+        $itemsData.forEach(function(itemData) {
+            console.log('loading', itemData);
+            $addItemToPage(itemData);
+        });
+    });
+
+
+
+    $('#add-form').submit(function(event) {
+      event.preventDefault();
+      let $itemDescription = event.target.itemDescription.value;
+      let $createTodo = $.ajax({
+          type: 'POST',
+          url: "http://listalous.herokuapp.com/lists/kmcelveen/items",
+          data: { description: $itemDescription, completed: false }
+      });
+
+      $createTodo.done(function(item) {
+          console.log('created todo', item);
+          $addItemToPage(item);
+          $('#create').val('');
+      });
+  });
+
   
-
-
-
-
-  
-})
+});
